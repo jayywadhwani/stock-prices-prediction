3761739be798b444cb1ed83dde0820d3b97b1f37
jayywadhwani <jaywadhwani6263779835@gmail.com>
2024-05-30 14:44:46 +0530

    first version v.1.0 of stock market prediction

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 
 @app.route('/',methods = ['GET','POST'])
 def predict():
-        try:
             symbol = 'SBIN'
             if request.form.get('input_text_symbol') != 'SBIN':
                   symbol= request.form.get('input_text_symbol')
@@ -13,8 +12,6 @@ def predict():
                   symbol = 'SBIN'
             predicted_value,mseerror,maerror,r2score,crscore,previousClose,todayOpen,dayHigh,dayLow,stock_symbol = analysis(symbol)
             return render_template('index.html',predicted_value=predicted_value,r2score=r2score,mseerror=mseerror,maeerror=maerror,vscore=crscore,Previous_Close=previousClose,Todays_Open=todayOpen,Day_High=dayHigh,Day_Low=dayLow,symbol=symbol.upper(),stock_symbol=stock_symbol)
-        except Exception as e:
-              return "Plesae Reload the page some error occured!!!"
 
 if __name__ == "__main__":
       app.run(debug=True)
