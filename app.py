4f4b716d7961650fda38353e37803c804298cda9
jayywadhwani <jaywadhwani6263779835@gmail.com>
2024-08-09 23:36:45 +0530

    minor space bug occurs some errors in code but now fixed

--- app.py
+++ app.py
@@ -8,11 +8,12 @@ def predict():
             symbol = 'SBIN'
             if request.form.get('input_text_symbol') != 'SBIN':
                   symbol= request.form.get('input_text_symbol')
-                  symbol = symbol.strip()
+                  if symbol is not None:
+                        symbol = symbol.strip()
             if request.form.get('input_text_symbol') == None or request.form.get('input_text_symbol')==  "" or request.form.get('input_text_symbol')==  "NULL":
                   symbol = 'SBIN'
             predicted_value,mseerror,maerror,r2score,crscore,previousClose,todayOpen,dayHigh,dayLow,stock_symbol = analysis(symbol)
             return render_template('index.html',predicted_value=predicted_value,r2score=r2score,mseerror=mseerror,maeerror=maerror,vscore=crscore,Previous_Close=previousClose,Todays_Open=todayOpen,Day_High=dayHigh,Day_Low=dayLow,symbol=symbol.upper(),stock_symbol=stock_symbol)
 
 if __name__ == "__main__":
-      app.run(debug=True)
+      app.run(debug=True,port=5000)
